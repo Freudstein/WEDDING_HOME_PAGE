4bc3b2edd83ab37810d9f8817808f80b4d2dcf7c
Alessio <prefetto.di.ferro@gmail.com>
2020-11-28 20:03:46 +0300

    HEADER

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-
+	$('.slider').slick({
+		autoplay: false,
+		speed: 30,
+		infinite: true,
+		fade: false,
+		prevArrow:$('.slider-nav__button--prev'),
+		nextArrow:$('.slider-nav__button--next'),
+	});
 });
