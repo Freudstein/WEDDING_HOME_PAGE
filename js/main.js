ba1889df0557f4bc5d754c8104081d95ca18f6d2
Alessio <prefetto.di.ferro@gmail.com>
2020-11-29 12:12:25 +0300

    Gallery + News&Events

--- js/main.js
+++ js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
 		prevArrow:$('.slider-nav__button--prev'),
 		nextArrow:$('.slider-nav__button--next'),
 	});
+
+	$('.gallery__slider').slick({
+		autoplay: true,
+		speed: 300,
+		infinite: true,
+		slidesToShow: 3,
+		prevArrow:$('.gallery__slider-nav .slider-nav__button--prev'),
+		nextArrow:$('.gallery__slider-nav .slider-nav__button--next'),
+	});
+
+	$('.gallery__slider-item-image').magnificPopup({
+		type: 'image',
+	});
 });
